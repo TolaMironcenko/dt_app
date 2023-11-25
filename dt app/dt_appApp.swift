defad013f380dfacf1a7b644d9497535c539c1f8
tola <79221297+TolaMironcenko@users.noreply.github.com>
2023-11-25 22:49:01 +0700

    Initial Commit

---
+++ dt app/dt_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  dt_appApp.swift
+//  dt app
+//
+//  Created by Анатолий Миронченко on 25.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct dt_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
